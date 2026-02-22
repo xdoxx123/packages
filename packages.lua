cb9fb41d4bda2ee60f3662724c3e8e85ffb1c2a1
xdoxx123 <xdoxx123@noreply.codeberg.org>
2026-02-22 19:13:21 +0100

    Update packages.lua

--- packages.lua
+++ packages.lua
@@ -2,7 +2,14 @@ return {
   ["skid"] = {
     name="poo",
     files={["main/bob.lua"]="/usr/bin"}
+  },
+  ["linknet"] = {
+    name="linknet",
+    files={["main/linknets/linknet.lua"]="/usr/bin"}
+  },
+  ["protnet"] = {
+    name="protnet",
+    files={["main/linknets/protnet.lua"]="/usr/bin"}
   }
-
 
 }
