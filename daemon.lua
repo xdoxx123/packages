94ecf015af44fc3f1c6e90f34bae6befe90c38a2
xdoxx123 <xdoxx123@noreply.codeberg.org>
2026-02-23 17:40:27 +0100

    Add daemon.lua

---
+++ daemon.lua
@@ -0,0 +1,23 @@
+local thread = require("thread")
+local fs = require("filesystem")
+local shell = require("shell")
+local args,options = shell.parse({...})
+
+if args[1] and options["P"] == true then
+    args[1] = filesystem.canonical(args[1])
+    local dookie = thread.create(function ()
+        pcall(function (...)
+            dofile(args[1])
+        end)
+    end)
+    dookie:detach()
+elseif args[1] then
+    args[1] = filesystem.canonical(args[1])
+    local dookie = thread.create(function ()
+        
+            dofile(args[1])
+     
+    end)
+    dookie:detach()
+end
+
